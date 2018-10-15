ccab79d1d2be8eb7af3037e867fbbcabac63d38d
Le Tuan <leanhtuan1998hl@gmail.com>
2018-10-15 07:38:38 +0700

    tdtu branch revision 2

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World I am TDTU");
+        System.out.println("Hello World This is TDTU");
     }
 }
