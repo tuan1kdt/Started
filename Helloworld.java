cac02bbe1410dcf00da094d667220a6a285484da
Le Tuan <leanhtuan1998hl@gmail.com>
2018-10-15 07:35:05 +0700

    master branch revision 3

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
+
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World I am Tuan");
+    	Scanner input = new Scanner( System.in );
+    	System.out.println("Enter your name: ");
+    	String user = input.nextLine();
+        System.out.println("Hello World I am "+ user);
     }
 }
